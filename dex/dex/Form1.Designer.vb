c144b2b453e49503ea7e2f55d6efafcfd1892595
Tanner <tkset494@learner.dvusd.org>
2024-01-11 08:07:26 -0700

    save data

---
+++ dex/dex/Form1.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.field1 = New System.Windows.Forms.TextBox()
+        Me.field2 = New System.Windows.Forms.TextBox()
+        Me.field3 = New System.Windows.Forms.TextBox()
+        Me.field4 = New System.Windows.Forms.TextBox()
+        Me.field5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip2 = New System.Windows.Forms.MenuStrip()
+        Me.RallycarsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 28)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'field1
+        '
+        Me.field1.Location = New System.Drawing.Point(390, 125)
+        Me.field1.Name = "field1"
+        Me.field1.Size = New System.Drawing.Size(100, 22)
+        Me.field1.TabIndex = 1
+        '
+        'field2
+        '
+        Me.field2.Location = New System.Drawing.Point(390, 153)
+        Me.field2.Name = "field2"
+        Me.field2.Size = New System.Drawing.Size(100, 22)
+        Me.field2.TabIndex = 2
+        '
+        'field3
+        '
+        Me.field3.Location = New System.Drawing.Point(390, 181)
+        Me.field3.Name = "field3"
+        Me.field3.Size = New System.Drawing.Size(100, 22)
+        Me.field3.TabIndex = 3
+        '
+        'field4
+        '
+        Me.field4.Location = New System.Drawing.Point(390, 209)
+        Me.field4.Name = "field4"
+        Me.field4.Size = New System.Drawing.Size(100, 22)
+        Me.field4.TabIndex = 4
+        '
+        'field5
+        '
+        Me.field5.Location = New System.Drawing.Point(390, 237)
+        Me.field5.Name = "field5"
+        Me.field5.Size = New System.Drawing.Size(100, 22)
+        Me.field5.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(311, 128)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 16)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Races won"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(311, 156)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 16)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Top speed"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(311, 184)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 16)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "0-60"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(311, 212)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 16)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "horsepower"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(311, 240)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 16)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "price"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 89)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(281, 265)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'MenuStrip2
+        '
+        Me.MenuStrip2.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RallycarsToolStripMenuItem})
+        Me.MenuStrip2.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip2.Name = "MenuStrip2"
+        Me.MenuStrip2.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip2.TabIndex = 12
+        Me.MenuStrip2.Text = "MenuStrip2"
+        '
+        'RallycarsToolStripMenuItem
+        '
+        Me.RallycarsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.SaveToolStripMenuItem})
+        Me.RallycarsToolStripMenuItem.Name = "RallycarsToolStripMenuItem"
+        Me.RallycarsToolStripMenuItem.Size = New System.Drawing.Size(85, 24)
+        Me.RallycarsToolStripMenuItem.Text = "Rally cars"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.field5)
+        Me.Controls.Add(Me.field4)
+        Me.Controls.Add(Me.field3)
+        Me.Controls.Add(Me.field2)
+        Me.Controls.Add(Me.field1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.MenuStrip2)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip2.ResumeLayout(False)
+        Me.MenuStrip2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents field1 As TextBox
+    Friend WithEvents field2 As TextBox
+    Friend WithEvents field3 As TextBox
+    Friend WithEvents field4 As TextBox
+    Friend WithEvents field5 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents MenuStrip2 As MenuStrip
+    Friend WithEvents RallycarsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ dex/dex/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        PictureBox1.Image = Nothing
+        field1.Text = ""
+        field2.Text = ""
+        field3.Text = ""
+        field4.Text = ""
+        field5.Text = ""
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim outFile As New IO.StreamWriter("data.txt")
+        outFile.Write(field1.Text)
+        outFile.Write("|")
+        outFile.Write(field2.Text)
+        outFile.Write("|")
+        outFile.Write(field3.Text)
+        outFile.Write("|")
+        outFile.Write(field4.Text)
+        outFile.Write("|")
+        outFile.Write(field5.Text)
+        outFile.Write("|")
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        PictureBox1.Load(OpenFileDialog1.FileName)
+    End Sub
+End Class
